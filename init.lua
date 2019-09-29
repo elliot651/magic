f293c8937d5e6e871580042743314a84ebcaeec9
elliot <elliotsmith651@gmail.com>
2019-09-29 16:57:57 -0700

    more stuff

--- init.lua
+++ init.lua
@@ -1,30 +1,45 @@
-minetest.register_craftitem("magic:wand", {
-
+minetest.register_craftitem("magic:magic_wand", {
+	stack_max = 1,
 	description = "wand",
 	inventory_image = "magic_wand.png"
 })
 
 minetest.register_craft({
 	type = "shaped",
-	output = "magic:wand",
+	output = "magic:magic_wand",
 	recipe = {
-		{"",              "",              "magic:spectral_crystal"},
+		{"",              "",              "magic:magic_spectral_crystal"},
 		{"",              "default:stick", ""},
 		{"default:stick", "",              ""}
 	}
 })
 
-minetest.register_node("magic:spectral_ore", {
-	description = "spectral_ore",
-	tiles = {"default_stone.png^spectral_ore.png"
+minetest.register_node("magic:magic_spectral_ore", {
+	description = "spectral ore",
+	tiles = {"default_stone.png^magic_spectral_ore.png"
 	},
 	is_ground_content = true,
 	groups = {cracky = 3},
-	drop = "magic:spectral_crystal"
+	drop = "magic:magic_spectral_crystal"
 })
 
-minetest.register_craftitem("magic:spectral_crystal", {
+minetest.register_craftitem("magic:magic_spectral_crystal", {
 
 	description = "spectral crystal",
-	inventory_image = "spectral_crystal.png"
+	inventory_image = "magic_spectral_crystal.png"
+})
+
+minetest.register_craftitem("magic:magic_danium_dust", {
+	description = "danium dust",
+	inventory_image = "magic_danium_dust.png"
+})
+
+minetest.register_craftitem("magic:magic_scroll", {
+	description = "scroll",
+	inventory_image = "magic_scroll.png"
+})
+
+minetest.register_craftitem("magic:magic_scroll_fireball", {
+	description = "fireball",
+	inventory_image = "magic_scroll_damage.png"
 })
